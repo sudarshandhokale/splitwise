a2def23ce07c4e53c3937837bed2978eb914f921
Sudarshan Dhokale <sudarshandhokale09@gmail.com>
2017-01-17 07:14:31 +0530

    final step are to be dome

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < Api::V1::ApiApplicationController
   def index
-    @users = User.all
+    @users = User.where.not(id: current_user.id)
     render json: success(@users), status: :ok
   end
 end
